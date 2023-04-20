c9e4c34f2e36606f903e66278d780ddc00aedcb3
Andrew Nascimento <81823870+AndrewPolengolas@users.noreply.github.com>
2023-04-20 17:03:30 -0300

    Ajustes

--- src/main/java/com/pi/feiradigital/security/SecurityConfig.java
+++ src/main/java/com/pi/feiradigital/security/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -39,9 +44,22 @@ public class SecurityConfig {
                         .requestMatchers("/vendedor/cadastro").permitAll()
                         .anyRequest().authenticated()
                         .and().addFilterBefore(authenticationSecurityFilter(), UsernamePasswordAuthenticationFilter.class)
-                );
+                )
+                .cors();
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // origens permitidas
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // métodos permitidos
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token")); // headers permitidos
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token")); // headers expostos
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
