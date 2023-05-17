2810d1dbe0061a327c51b5e44e93d54d9b6b260e
dasdiego <diegogoigle@gmail.com>
2023-05-17 20:54:02 -0300

    Ajuste dos Cors

--- src/main/java/com/pi/feiradigital/FeiraDigitalApplication.java
+++ src/main/java/com/pi/feiradigital/FeiraDigitalApplication.java
@@ -6,11 +6,29 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @SpringBootApplication
 public class FeiraDigitalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FeiraDigitalApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*"); // Permite qualquer origem
+        config.addAllowedMethod("*"); // Permite qualquer método (GET, POST, etc)
+        config.addAllowedHeader("*"); // Permite qualquer cabeçalho
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
